363574523838eab8263673ebbb55f66871e221dc
Vicky <kosenchiha@gmail.com>
2020-02-26 13:27:59 +0000

    Prevent user to register with the same email twice

--- routes/auth.js
+++ routes/auth.js
@@ -6,6 +6,10 @@ router.post("/register", async (req, res) => {
   const { error } = registerValidation(req.body);
   if (error) return res.status(400).send(error.details[0].message);
 
+  const emailExist = await User.findOne({ email: req.body.email });
+  if (emailExist)
+    return res.status(400).send("User with this email already registered");
+
   const user = new User({
     name: req.body.name,
     email: req.body.email,
