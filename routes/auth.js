f1d2aefc4976542057e077ae64887ec434542482
Vicky <kosenchiha@gmail.com>
2020-02-26 13:21:33 +0000

    Add validation for user credentials

--- routes/auth.js
+++ routes/auth.js
@@ -1,7 +1,11 @@
 const router = require("express").Router();
 const User = require("../model/User");
+const { registerValidation, loginValidation } = require("./validation");
 
 router.post("/register", async (req, res) => {
+  const { error } = registerValidation(req.body);
+  if (error) return res.status(400).send(error.details[0].message);
+
   const user = new User({
     name: req.body.name,
     email: req.body.email,
@@ -10,8 +14,8 @@ router.post("/register", async (req, res) => {
   try {
     const savedUser = await user.save();
     res.send(savedUser);
-  } catch (err) {
-    res.status(400).send(err);
+  } catch (error) {
+    res.status(400).send(error);
   }
 });
 
---
+++ routes/validation.js
@@ -0,0 +1,32 @@
+const Joi = require("@hapi/joi");
+
+const registerValidation = data => {
+  const schema = Joi.object({
+    name: Joi.string()
+      .min(6)
+      .required(),
+    email: Joi.string()
+      .min(6)
+      .required()
+      .email(),
+    password: Joi.string()
+      .min(6)
+      .required()
+  });
+  return schema.validate(data);
+};
+
+const loginValidation = data => {
+  const schema = {
+    email: Joi.string()
+      .min(6)
+      .required()
+      .email(),
+    password: Joi.string()
+      .min(6)
+      .required()
+  };
+  return Joi.validate(data, schema);
+};
+
+module.exports = { registerValidation, loginValidation };
